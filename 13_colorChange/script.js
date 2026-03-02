075a285c2368af14aaf3e71840902f2258d66380
RamjiDeveloper <ramji54902@gmail.com>
2026-03-02 14:35:09 +0530

    new file added

---
+++ 13_colorChange/script.js
@@ -0,0 +1,32 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+
+
+buttons.forEach( (button) => {
+    // console.log(button);
+    button.addEventListener('click', (e) => {
+        // console.log(e);
+        // console.log(e.target)
+        switch (e.target.id) {
+            case 'white':
+                body.style.background = 'red'
+                break;
+            case 'gray':
+                body.style.background = 'green'
+                break;
+            case 'blue':
+                body.style.background = 'orange'
+                break;
+            case 'yellow':
+                body.style.background = 'skyBlue'
+                break;
+        
+            default:
+                break;
+        }
+        
+    })
+    
+})
+
